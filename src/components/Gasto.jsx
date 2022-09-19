58015b72d708061d9028d327fb56325f6310a7dd
Emmanuel Zocco <la130bis@gmail.com>
2022-09-18 21:24:33 -0300

    app control presupueto

---
+++ src/components/Gasto.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import {
+    LeadingActions,
+    SwipeableList,
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions,
+  } from 'react-swipeable-list';
+  import 'react-swipeable-list/dist/styles.css';
+import { formatearFecha } from '../helpers'
+
+import IconoAhorro from '../img/icono-ahorro.jpeg'
+import IconoCasa from '../img/icono-casa.jpeg'
+import IconoComida from '../img/icono-comida.jpeg'
+import IconoGastos from '../img/icono-gastos.jpeg'
+import IconoOcio from '../img/icono-ocio.jpeg'
+import IconoSalud from '../img/icono-salud.jpeg'
+import IconoSuscripciones from '../img/icono-suscripciones.jpeg'
+
+const diccionarioIconos = {
+ahorro:IconoAhorro,
+comida:IconoComida,
+casa:IconoCasa,
+gastos:IconoGastos,
+ocio:IconoOcio,
+salud:IconoSalud,
+suscripciones:IconoSuscripciones,
+}
+
+const Gasto = ({gasto, setGastoEditar, eliminarGasto}) => {
+    const {categoria, nombre, cantidad, id, fecha} = gasto
+
+    const leadingActions = ()=>(
+      <LeadingActions>
+      <SwipeAction onClick={()=> setGastoEditar(gasto)}>
+       Editar
+      </SwipeAction>
+    </LeadingActions>
+    )
+    const trailingActions = ()=>(
+      <TrailingActions>
+        <SwipeAction 
+        onClick={()=> eliminarGasto(id)}
+        destructive={true}
+        >
+         Eliminar
+        </SwipeAction>
+      </TrailingActions>
+    )
+  return (
+    <SwipeableList>
+        <SwipeableListItem 
+        leadingActions={leadingActions()}
+        trailingActions={trailingActions()} 
+        >
+
+    <div className='gasto sombra'>
+    <div className='contenido-gasto'>
+        <img 
+        src={diccionarioIconos[categoria]}
+        alt='Icono-Gasto'
+        />
+        <div className='descripcion-gasto'>
+          <p className='categoria'> {categoria} </p>  
+          <p className='nombre-gasto'> {nombre} </p>  
+          <p className='fecha-gasto'>Agregado el:  {''}
+          <span>{formatearFecha(fecha)}</span>
+          </p> 
+           
+        </div>
+
+    </div>
+    <p className='cantidad-gasto'> $ {cantidad} </p>
+    </div>
+    </SwipeableListItem>
+    </SwipeableList>
+  )
+}
+
+export default Gasto
